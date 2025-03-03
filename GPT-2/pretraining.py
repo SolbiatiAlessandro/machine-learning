[gpt2] gradient accumulation fix

GPT-2/pretraining.py
@@ -12,7 +12,7 @@ from utils import device, get_free_gpu_memory, LossLogs, save_checkpoint, load_c
 
 config = GPTConfig()
 config.mini_batch_size = 64
-config.total_batch_size = 64 * 1000
+config.total_batch_size = 524288
 config.block_size = 1024
 config.epochs = 1000000
 config.validation_frequency = 10
@@ -20,7 +20,7 @@ config.validation_epochs = 5
 config.dataset = "wikitext"
 config.tokenizer_name = "wikitext2_18k"
 config.downstream_evals_iterations = 300
-config.downstream_evals_frequency = 200
+config.downstream_evals_frequency = 100
 
 import wandb
 import random
@@ -104,99 +104,109 @@ def get_lr(step):
     return min_lr
 
     
-def get_batch_size(step, start_batch_size = 32 * 4):
+def get_batch_size(step, start_batch_size = 64):
     if step < warmup_step:
         return start_batch_size + int((config.total_batch_size - start_batch_size) * (step+1) / warmup_step)
     return config.total_batch_size
-
-batch_accumulator = 0
 
 from time import time
 train_epoch = 0
 optimizer.zero_grad()
 while True:
-    t0 = time()
     
+    train_epoch += 1
+    total_batch_iteration_time = 0
     batch_size = get_batch_size(train_epoch)
-    batch_accumulator += config.mini_batch_size
-    bb = config.mini_batch_size * config.block_size
-    X, y = data_loader.next_batch(device=device, batch_size=config.mini_batch_size)
-    t01 = time()
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits = model(X)
-        train_loss = F.cross_entropy(logits.view(bb, -1), y.view(bb))
-    train_loss.backward()
-    
+    epoch_train_loss = 0.0
+    t0 = time()
+    mini_batch_steps = int(batch_size / config.mini_batch_size)
+    for mini_batch_step in range(mini_batch_steps):
         
-    torch.cuda.synchronize()
+        
+    
+        bb = config.mini_batch_size * config.block_size
+        X, y = data_loader.next_batch(device=device, batch_size=config.mini_batch_size)
+        t01 = time()
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits = model(X)
+            train_loss = F.cross_entropy(logits.view(bb, -1), y.view(bb))
+            
+        train_loss = train_loss / mini_batch_steps
+        epoch_train_loss += train_loss.detach()
+        train_loss.backward()  
+        
+        torch.cuda.synchronize()
+        
     t1 = time()
     dt = (t1 - t0) * 1000 
-    dt0 = (t01 - t0) * 1000
-    tps = 1000*X.shape[0]*X.shape[1]/dt
-    
+    tps = 1000*X.shape[0]*X.shape[1]*mini_batch_steps/dt
+        
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    lr = get_lr(train_epoch*mini_batch_steps)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    optimizer.step()
+    optimizer.zero_grad()
+
     infra_metrics = {
-            'infra/iteration_time(ms)': dt,
-            'infra/data_loader_time(ms)': dt0,
-            'infra/tokens_per_second': tps,
-            #'training/norm': norm,
-            'training/batch_size': batch_size,
-            #'training/learning_rate': lr,
-            'training/batch_accumulator': batch_accumulator,
-        }
-    
-    if batch_accumulator >= config.total_batch_size:
-        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        lr = get_lr(train_epoch)
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-        train_epoch += 1
-        train_loss = train_loss / batch_accumulator
-        optimizer.step()
-        optimizer.zero_grad()
-        batch_accumulator = 0
-        NTPloss.log_train(train_epoch, train_loss.item(), infra_metrics=infra_metrics if train_epoch > 5 else None)
-
-        if train_epoch >= config.downstream_evals_frequency and (train_epoch % config.downstream_evals_frequency == 0 or train_epoch % (config.downstream_evals_frequency + 1) == 0):
-            accuracy, _, skipped = evaluate_downstream_cbt_with_probs(
-                model=model,
-                tokenizer=data_loader.tokenizer,
-                device=device,
-                dataset_split="validation",
-                verbose=False,
-                max_context_length=config.block_size - 1,
-                max_examples=config.downstream_evals_iterations  
-            )
-            wandb.log({
-                'downstream/children_book_text_accuracy': accuracy,
-                'downstream/children_book_text_skipped': skipped,
-            })
-
-            generated_evals = sample_generations(
-                model, 
-                data_loader.tokenizer, 
-                config, 
-                device=device,
-                wandb_obj=wandb,
-                iteration=train_epoch)
+        'infra/iteration_time(ms)': dt,
+        'infra/tokens_per_second': tps,
+        'training/norm': norm,
+        'training/batch_size': batch_size,
+        'training/learning_rate': lr,
+        'training/mini_batch_steps': mini_batch_steps,
+    }
+    NTPloss.log_train(train_epoch, epoch_train_loss.item(), infra_metrics=infra_metrics)
 
 
-        if (train_epoch + 1) % config.validation_frequency == 0:
-            model.eval()
-            with torch.no_grad():
-                epoch_val_losses = []
-                for val_epoch in range(config.validation_epochs):
-                    X, y = data_loader.next_batch(mode="eval", device=device, batch_size=config.mini_batch_size)
-                    logits = model(X)
-                    val_loss = F.cross_entropy(logits.view(bb, -1), y.view(bb))
-                    NTPloss.log_val(train_epoch, val_epoch, val_loss.item())
-
-                    del X, y, logits
-                    gc.collect()
-                    torch.cuda.empty_cache()
+    loss_string = f"[{train_epoch}*{mini_batch_steps}/{config.epochs}] train_loss={epoch_train_loss.item():.3f}"
+    infra_string = ", ".join([f"{k}={v:.6f}" for k,v in infra_metrics.items()])
+    print(loss_string + infra_string)
 
 
-                    model.train()
-                    loss_string = f"[{train_epoch}/{config.epochs}] train_loss={train_loss.item():.3f},val_loss={NTPloss.get_val_loss(train_epoch):.3f}, "
-                    loss_string += f", norm={norm}"
-                    infra_string = ", ".join([f"{k}={v:.3f}" for k,v in infra_metrics.items()])
-                    print(loss_string + infra_string)
+
+    if train_epoch >= config.downstream_evals_frequency and (train_epoch % config.downstream_evals_frequency == 0 or train_epoch % (config.downstream_evals_frequency + 1) == 0):
+        accuracy, _, skipped = evaluate_downstream_cbt_with_probs(
+            model=model,
+            tokenizer=data_loader.tokenizer,
+            device=device,
+            dataset_split="validation",
+            verbose=False,
+            max_context_length=config.block_size - 1,
+            max_examples=config.downstream_evals_iterations  
+        )
+        wandb.log({
+            'downstream/children_book_text_accuracy': accuracy,
+            'downstream/children_book_text_skipped': skipped,
+        })
+
+        generated_evals = sample_generations(
+            model, 
+            data_loader.tokenizer, 
+            config, 
+            device=device,
+            wandb_obj=wandb,
+            iteration=train_epoch)
+
+
+    if (train_epoch + 1) % config.validation_frequency == 0:
+        model.eval()
+        with torch.no_grad():
+            epoch_val_losses = []
+            for val_epoch in range(config.validation_epochs):
+                X, y = data_loader.next_batch(mode="eval", device=device, batch_size=config.mini_batch_size)
+                logits = model(X)
+                val_loss = F.cross_entropy(logits.view(bb, -1), y.view(bb))
+                NTPloss.log_val(train_epoch, val_epoch, val_loss.item())
+
+                del X, y, logits
+                gc.collect()
+                torch.cuda.empty_cache()
+
+
+                model.train()
+                loss_string = f"[{train_epoch}/{config.epochs}] train_loss={epoch_train_loss.item():.3f},val_loss={NTPloss.get_val_loss(train_epoch):.3f}, "
+                loss_string += f", norm={norm}"
+                infra_string = ", ".join([f"{k}={v:.3f}" for k,v in infra_metrics.items()])
+                print(loss_string + infra_string)